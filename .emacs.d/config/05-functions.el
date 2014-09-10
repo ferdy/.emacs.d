18d41e88b2aa74f2bf5f7d6663bd0db1a26def8a
boccaperta-it <manuel@boccaperta.com>
2014-09-10 19:48:12 +0200

    refactoring

--- .emacs.d/config/05-functions.el
+++ .emacs.d/config/05-functions.el
@@ -62,23 +62,3 @@
     (interactive)
     (revert-buffer nil t t)
     (message (concat "Reverted buffer " (buffer-name))))
-
-;; Searching buffers with occur mode
-(eval-when-compile
-  (require 'cl))
-
-(defun get-buffers-matching-mode (mode)
-  "Returns a list of buffers where their major-mode is equal to MODE"
-  (let ((buffer-mode-matches '()))
-   (dolist (buf (buffer-list))
-     (with-current-buffer buf
-       (if (eq mode major-mode)
-           (add-to-list 'buffer-mode-matches buf))))
-   buffer-mode-matches))
-
-(defun multi-occur-in-this-mode ()
-  "Show all lines matching REGEXP in buffers with this major mode."
-  (interactive)
-  (multi-occur
-   (get-buffers-matching-mode major-mode)
-   (car (occur-read-primary-args))))
