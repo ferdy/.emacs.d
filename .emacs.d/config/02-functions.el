39516b3a20c9c2f60d441f9fde04877aeb17f993
boccaperta-it <manuel@boccaperta.com>
2014-12-17 20:05:43 +0100

    copy-file-name-to-clipboard

--- .emacs.d/config/02-functions.el
+++ .emacs.d/config/02-functions.el
@@ -121,3 +121,15 @@ a new instance."
 		   '(lambda()
 		      (interactive)
 		      (custom/term-start-or-switch ,name ,use-existing))))
+
+;; Copy current file name to clipboard
+;; See: http://emacsredux.com/blog/2013/03/27/copy-filename-to-the-clipboard/
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
