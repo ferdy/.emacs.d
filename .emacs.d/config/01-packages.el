c1eae9aa8d37d5403bf228457ab942047023b99f
boccaperta-it <manuel@boccaperta.com>
2014-12-19 20:11:22 +0100

    removed marmalade

--- .emacs.d/config/01-packages.el
+++ .emacs.d/config/01-packages.el
@@ -11,9 +11,6 @@
 ;; Add the original Emacs Lisp Package Archive
 (add-to-list 'package-archives
              '("elpa" . "http://tromey.com/elpa/"))
-;; Add the user-contributed repository
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
 ;; Add Melpa
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/") t)
