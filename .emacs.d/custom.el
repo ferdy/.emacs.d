f4dedd48d4e3c5b94e62edd3ae89a4819c9a2002
boccaperta-it <manuel@boccaperta.com>
2014-12-20 11:27:38 +0100

    refactoring

---
+++ .emacs.d/custom.el
@@ -0,0 +1,26 @@
+;;; custom.el --- Part of my Emacs configuration
+
+;;; Commentary:
+;; First file to be loaded when Emacs starts.
+
+;;; Code:
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "/home/manuel/.emacs.d/bookmarks")
+ '(custom-safe-themes
+   (quote
+    ("c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
+ '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
+ '(magit-use-overlays nil)
+ '(paradox-github-token t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;; custom.el ends here
