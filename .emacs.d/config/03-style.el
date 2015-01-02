debc37d95462e2abe8795522078224203f392c66
boccaperta-it <manuel@boccaperta.com>
2015-01-02 18:26:14 +0100

    imenu-anywhere

--- .emacs.d/config/03-style.el
+++ .emacs.d/config/03-style.el
@@ -154,58 +154,6 @@
 	  ido-default-item nil
 	  ido-cur-list nil)
 
-    ;; Ido-charged version of imenu
-    ;; See http://www.emacswiki.org/emacs/ImenuMode#toc13
-    (defun ido-goto-symbol (&optional symbol-list)
-      "Refresh imenu and jump to a place in the buffer not in SYMBOL-LIST using Ido."
-      (interactive)
-      (unless (featurep 'imenu)
-	(require 'imenu nil t))
-      (cond
-       ((not symbol-list)
-	(let ((ido-mode ido-mode)
-	      (ido-enable-flex-matching
-	       (if (boundp 'ido-enable-flex-matching)
-		   ido-enable-flex-matching t))
-	      name-and-pos symbol-names position)
-	  (unless ido-mode
-	    (ido-mode 1)
-	    (setq ido-enable-flex-matching t))
-	  (while (progn
-		   (imenu--cleanup)
-		   (setq imenu--index-alist nil)
-		   (ido-goto-symbol (imenu--make-index-alist))
-		   (setq selected-symbol
-			 (ido-completing-read "Symbol? " symbol-names))
-		   (string= (car imenu--rescan-item) selected-symbol)))
-	  (unless (and (boundp 'mark-active) mark-active)
-	    (push-mark nil t nil))
-	  (setq position (cdr (assoc selected-symbol name-and-pos)))
-	  (cond
-	   ((overlayp position)
-	    (goto-char (overlay-start position)))
-	   (t
-	    (goto-char position)))))
-       ((listp symbol-list)
-	(dolist (symbol symbol-list)
-	  (let (name position)
-	    (cond
-	     ((and (listp symbol) (imenu--subalist-p symbol))
-	      (ido-goto-symbol symbol))
-	     ((listp symbol)
-	      (setq name (car symbol))
-	      (setq position (cdr symbol)))
-	     ((stringp symbol)
-	      (setq name symbol)
-	      (setq position
-		    (get-text-property 1 'org-imenu-marker symbol))))
-	    (unless (or (null position) (null name)
-			(string= (car imenu--rescan-item) name))
-	      (add-to-list 'symbol-names name)
-	      (add-to-list 'name-and-pos (cons name position))))))))
-
-    (global-set-key (kbd "M-i") 'ido-goto-symbol)
-
     ;; Ido bury buffer
     ;; See http://endlessparentheses.com/Ido-Bury-Buffer.html
     (add-hook
@@ -252,6 +200,11 @@
   :ensure t
   :bind (([remap execute-extended-command] . smex)
 	 ("M-X" . smex-major-mode-commands)))
+
+;; IMENU-ANYWHERE
+(use-package imenu-anywhere
+  :ensure t
+  :bind (("M-I" . imenu-anywhere)))
 
 ;; Tune `eval-expression'
 (add-hook 'eval-expression-minibuffer-setup-hook #'eldoc-mode)
