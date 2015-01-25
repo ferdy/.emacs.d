4f8d7165586c8d93f3e9ea51c8ec847702556b77
boccaperta-it <manuel@boccaperta.com>
2015-01-25 11:22:49 +0100

    iedit-dwim

--- .emacs.d/config/03-style.el
+++ .emacs.d/config/03-style.el
@@ -461,7 +461,27 @@
 
 ;; IEDIT
 (use-package iedit
-  :ensure t)
+  :ensure t
+  :config
+  (progn
+    ;; See: http://www.masteringemacs.org/article/iedit-interactive-multi-occurrence-editing-in-your-buffer
+    (defun iedit-dwim (arg)
+      "Starts iedit but uses \\[narrow-to-defun] to limit its scope."
+      (interactive "P")
+      (if arg
+	  (iedit-mode)
+	(save-excursion
+	  (save-restriction
+	    (widen)
+	    ;; this function determines the scope of `iedit-start'.
+	    (if iedit-mode
+		(iedit-done)
+	      ;; `current-word' can of course be replaced by other
+	      ;; functions.
+	      (narrow-to-defun)
+	      (iedit-start (current-word) (point-min) (point-max)))))))
+
+    (global-set-key (kbd "C-;") 'iedit-dwim)))
 
 ;; EXPAND-REGION
 (use-package expand-region
