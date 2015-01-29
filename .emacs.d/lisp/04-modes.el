38983b1357074ab0e0d1665e3fa2d63297fd2f3b
boccaperta-it <manuel@boccaperta.com>
2015-01-29 18:33:45 +0100

    no quickhelp

--- .emacs.d/lisp/04-modes.el
+++ .emacs.d/lisp/04-modes.el
@@ -604,12 +604,6 @@
 			  company-backends)))
     (add-hook 'TeX-mode-hook 'my-latex-mode-setup)))
 
-;; Help popup when idling on a completion candidate
-(use-package company-quickhelp
-  :ensure t
-  :defer t
-  :idle (company-quickhelp-mode 1))
-
 ;; UNDO-TREE
 (use-package undo-tree
   :ensure t
