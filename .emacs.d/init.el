5e4826947423517041d7c596ec458d35d8d43021
boccaperta-it <manuel@boccaperta.com>
2015-03-17 14:57:50 +0100

    el-get from melpa, only needed once for po-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -47,15 +47,10 @@
   (require 'use-package))
 
 ;; Add el-get
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
+(use-package el-get
+  :ensure t
+  :disabled t
+  :init (el-get 'sync))
 
 ;;; Initialization
 (when (version< emacs-version "25")
