e042d55848ccbabefc794094fab1d4aee31e0cd2
boccaperta-it <manuel@boccaperta.com>
2015-03-18 17:49:42 +0100

    defer aggressive-fill-paragraph

--- .emacs.d/lisp/03-editing.el
+++ .emacs.d/lisp/03-editing.el
@@ -181,9 +181,10 @@
 
 (use-package aggressive-fill-paragraph ; Automatically fill paragrah
   :ensure t
-  :init (progn
-          (add-hook 'org-mode-hook #'aggressive-fill-paragraph-mode)
-          (add-hook 'TeX-mode-hook #'aggressive-fill-paragraph-mode)))
+  :defer t
+  :config (progn
+            (add-hook 'org-mode-hook #'aggressive-fill-paragraph-mode)
+            (add-hook 'TeX-mode-hook #'aggressive-fill-paragraph-mode)))
 
 (use-package visual-fill-column ; Wrap at fill column
   :ensure t
