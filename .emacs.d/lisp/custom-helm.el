062920d85418cb9417d698a9d92436d44d4ac0eb
Manuel Uberti <manuel@boccaperta.com>
2015-04-28 17:13:10 +0200

    Cleanup helm-swoop config

--- .emacs.d/lisp/custom-helm.el
+++ .emacs.d/lisp/custom-helm.el
@@ -158,22 +158,17 @@
             (define-key helm-multi-swoop-map (kbd "C-s") 'helm-next-line)
 
             ;; Save buffer when helm-multi-swoop-edit complete
-            (setq helm-multi-swoop-edit-save t)
-
-            ;; If this value is t, split window inside the current window
-            (setq helm-swoop-split-with-multiple-windows nil)
-
-            ;; Split direction.
-            (setq helm-swoop-split-direction 'split-window-horizontally)
-
-            ;; If nil, you can boost invoke speed in exchange for text color
-            (setq helm-swoop-speed-or-color nil)
-
-            ;; Go to the opposite side of line from the end or beginning of line
-            (setq helm-swoop-move-to-line-cycle t)
-
-            ;; Optional face for line numbers
-            (setq helm-swoop-use-line-number-face t)))
+            (setq helm-multi-swoop-edit-save t
+                  ;; If this value is t, split window inside the current window
+                  helm-swoop-split-with-multiple-windows nil
+                  ;; Split direction
+                  helm-swoop-split-direction 'split-window-horizontally
+                  ;; If nil, boost invoke speed in exchange for text color
+                  helm-swoop-speed-or-color nil
+                  ;; Go to the opposite side from the end or beginning of line
+                  helm-swoop-move-to-line-cycle t
+                  ;; Optional face for line numbers
+                  helm-swoop-use-line-number-face t)))
 
 (use-package helm-company ; Show Company candidates through Helm interface
   :ensure t
