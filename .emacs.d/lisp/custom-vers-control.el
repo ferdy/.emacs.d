9fbe6743d2357cd97bcdbbdab7fff926089772f6
Manuel Uberti <manuel@boccaperta.com>
2015-04-28 10:18:46 +0200

    Cleanup Springboard buffer when Magit quits

--- .emacs.d/lisp/custom-vers-control.el
+++ .emacs.d/lisp/custom-vers-control.el
@@ -33,9 +33,10 @@
               (delete-other-windows))
 
             (defun magit-quit-session ()
-              "Restore previous window configuration and kill magit buffer."
+              "Restore previous window configuration and cleanup buffers."
               (interactive)
               (custom/kill-buffers "^\\*magit")
+              (custom/kill-buffers "^\\*Springboard")
               (tramp-cleanup-all-buffers)
               (tramp-cleanup-all-connections)
               (jump-to-register :magit-fullscreen))
