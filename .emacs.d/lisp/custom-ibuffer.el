4fcf9e396f30240bde782d0be8c8b601c916921d
Manuel Uberti <manuel@boccaperta.com>
2015-05-12 15:32:42 +0200

    Add ibuffer-projectile

--- .emacs.d/lisp/custom-ibuffer.el
+++ .emacs.d/lisp/custom-ibuffer.el
@@ -50,6 +50,10 @@
                     (unless (eq ibuffer-sorting-mode 'alphabetic)
                       (ibuffer-do-sort-by-alphabetic)))))
 
+(use-package ibuffer-projectile         ; Group buffers by Projectile project
+  :ensure t
+  :defer t)
+
 (provide 'custom-ibuffer)
 
 ;;; custom-ibuffer.el ends here
