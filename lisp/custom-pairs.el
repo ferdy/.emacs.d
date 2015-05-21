3a5da921ba629e97c99bf312c7ae796ec1ea6faf
Manuel Uberti <manuel@boccaperta.com>
2015-05-21 16:06:53 +0200

    Use Paredit, remove Smartparens

--- lisp/custom-pairs.el
+++ lisp/custom-pairs.el
@@ -10,67 +10,9 @@
 ;; This file stores pairs balancing configuration
 
 ;;; Code:
-(use-package smartparens ; Parenthesis editing and balancing
-  :ensure t
-  :init (progn
-          (smartparens-global-mode)
-          (show-smartparens-global-mode)
-
-          (dolist (hook '(inferior-emacs-lisp-mode-hook
-                          emacs-lisp-mode-hook
-                          clojure-mode-hook))
-            (add-hook hook #'smartparens-strict-mode)))
-  :config (progn
-            (setq sp-autoskip-closing-pair 'always
-                  ;; Don't kill entire symbol on C-k
-                  sp-hybrid-kill-entire-symbol nil)
-
-            (use-package smartparens-config
-              :config (progn
-                        (sp-local-pair '(emacs-lisp-mode
-                                         lisp-interaction-mode
-                                         inferior-emacs-lisp-mode)
-                                       "(" nil :bind "M-(")
-
-                        (bind-keys
-                         :map smartparens-mode-map
-                         ;; Movement and navigation
-                         ("C-M-f"       . sp-forward-sexp)
-                         ("C-M-b"       . sp-backward-sexp)
-                         ("C-M-u"       . sp-backward-up-sexp)
-                         ("C-M-d"       . sp-down-sexp)
-                         ("C-M-p"       . sp-backward-down-sexp)
-                         ("C-M-n"       . sp-up-sexp)
-                         ;; Deleting and killing
-                         ("C-M-k"       . sp-kill-sexp)
-                         ("C-M-w"       . sp-copy-sexp)
-                         ;; Depth changing
-                         ("M-S-<up>"    . sp-splice-sexp)
-                         ("M-<up>"      . sp-splice-sexp-killing-backward)
-                         ("M-<down>"    . sp-splice-sexp-killing-forward)
-                         ("M-C-<up>"    . sp-splice-sexp-killing-around)
-                         ("M-?"         . sp-convolute-sexp)
-                         ;; Barfage & Slurpage
-                         ("C-)"         . sp-forward-slurp-sexp)
-                         ("C-<right>"   . sp-forward-slurp-sexp)
-                         ("C-}"         . sp-forward-barf-sexp)
-                         ("C-<left>"    . sp-forward-barf-sexp)
-                         ("C-("         . sp-backward-slurp-sexp)
-                         ("C-M-<left>"  . sp-backward-slurp-sexp)
-                         ("C-{"         . sp-backward-barf-sexp)
-                         ("C-M-<right>" . sp-backward-barf-sexp)
-                         ;; Miscellaneous commands
-                         ("M-S"         . sp-split-sexp)
-                         ("M-J"         . sp-join-sexp)
-                         ("C-M-t"       . sp-transpose-sexp))
-
-                        (bind-key "M-q" #'sp-indent-defun
-                                  smartparens-strict-mode-map)))))
-
 (use-package paredit ; Balanced sexp editing
   :ensure t
   :defer t
-  :disabled t
   :init (dolist (hook '(eval-expression-minibuffer-setup-hook
                         emacs-lisp-mode-hook
                         inferior-emacs-lisp-mode-hook
