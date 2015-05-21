9c13271e4306baab1598bb90c7a83eb53a49d44b
Manuel Uberti <manuel@boccaperta.com>
2015-05-21 19:41:23 +0200

    Refactor in-buffer, frame and window navigation

--- lisp/custom-editing.el
+++ lisp/custom-editing.el
@@ -148,34 +148,6 @@
   :config (setq copyright-year-ranges t
                 copyright-names-regexp (regexp-quote user-full-name)))
 
-;;; Windows and frames
-(use-package winner ; Undo and redo window configurations
-  :init (winner-mode))
-
-(use-package avy-jump ; Jump to characters in buffers
-  :ensure avy
-  :bind (("C-c j s" . avy-isearch)
-         ("C-c j w" . avy-goto-word-1)
-         ("C-c j j" . avy-goto-char-2))
-  :config (setq avy-keys ; Use home row
-                '(?a ?s ?d ?e ?f ?h ?j ?k ?l ?n ?m ?v ?r ?u)))
-
-(use-package ace-link ; Jump to links
-  :ensure t
-  :defer t
-  :init (progn (with-eval-after-load 'info
-                 (bind-key "C-c j l" #'ace-link-info Info-mode-map))
-               (with-eval-after-load 'help-mode
-                 (defvar help-mode-map) ; Silence the byte compiler
-                 (bind-key "C-c j l" #'ace-link-help help-mode-map))))
-
-(use-package ace-window ; Better movements between windows
-  :ensure t
-  :bind ("C-x o" . ace-window)
-  :config (setq aw-keys ; Use home row
-                '(?a ?s ?d ?f ?g ?h ?j ?k ?l)
-                aw-dispatch-always t))
-
 (provide 'custom-editing)
 
 ;;; custom-editing.el ends here
