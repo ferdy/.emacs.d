1c48bbe2b30370a0b37b7a30cab70274c01990e0
Manuel <manuel@boccaperta.com>
2015-06-11 20:27:12 +0200

    Fix iedit configuration

--- lisp/custom-editing.el
+++ lisp/custom-editing.el
@@ -49,24 +49,7 @@
 
 (use-package iedit ; Edit multiple occurrences
   :ensure t
-  :config (progn
-            (defun iedit-dwim (arg)
-              "Starts iedit but uses \\[narrow-to-defun] to limit its scope."
-              (interactive "P")
-              (if arg
-                  (iedit-mode)
-                (save-excursion
-                  (save-restriction
-                    (widen)
-                    ;; this function determines the scope of `iedit-start'.
-                    (if iedit-mode
-                        (iedit-done)
-                      ;; `current-word' can of course be replaced by other
-                      ;; functions.
-                      (narrow-to-defun)
-                      (iedit-start (current-word) (point-min) (point-max)))))))
-
-            (global-set-key (kbd "C-,") 'iedit-dwim)))
+  :bind ("C-," . iedit-mode))
 
 (use-package adaptive-wrap ; Better line wrap
   :ensure t
