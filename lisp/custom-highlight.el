a6d735d2316ba8bae5745844ce8d854961939d4d
Manuel <manuel@boccaperta.com>
2015-07-01 19:13:39 +0200

    Enable rainbow-delimiters only for prog-mode

--- lisp/custom-highlight.el
+++ lisp/custom-highlight.el
@@ -57,8 +57,7 @@
 (use-package rainbow-delimiters ; Highlight parens
   :ensure t
   :defer t
-  :init (dolist (hook '(text-mode-hook prog-mode-hook))
-          (add-hook hook #'rainbow-delimiters-mode)))
+  :init (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
 (use-package hl-line ; Highlight current line
   :init (global-hl-line-mode 1))
