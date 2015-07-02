a273cf66b66258cb601f0288d8f3d02b02ad4fc1
Manuel <manuel@boccaperta.com>
2015-07-02 19:18:44 +0200

    Add keybinding for ace-swap-window

--- lisp/custom-navigation.el
+++ lisp/custom-navigation.el
@@ -39,7 +39,8 @@
 
 (use-package ace-window ; Better movements between windows
   :ensure t
-  :bind ("C-x o" . ace-window)
+  :bind (("C-x o"   . ace-window)
+         ("C-c n s" . ace-swap-window))
   :config (setq aw-keys ; Use home row
                 '(?a ?s ?d ?f ?g ?h ?j ?k ?l)
                 aw-dispatch-always t))
