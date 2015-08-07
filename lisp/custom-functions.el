2768ee9304df7b6238cc7bd4b35f5dacf3499c23
Manuel <manuel@boccaperta.com>
2015-08-07 15:12:28 +0200

    Add command to count lines of code
    See: https://github.com/lunaryorn/.emacs.d/commit/8b7e8a2969f413ef93bd6583f46dc7f189e6de63

--- lisp/custom-functions.el
+++ lisp/custom-functions.el
@@ -293,6 +293,40 @@ region if active."
   (let ((fill-column (point-max)))
     (fill-paragraph nil region)))
 
+(defun custom/all-init-files (&optional with-packages)
+  "Return a list of all Emacs Lisp files in my configuration.
+
+If WITH-PACKAGES is given and non-nil include 3rd party
+packages."
+  (append (list user-init-file)
+          (directory-files-recursively (locate-user-emacs-file "lisp/")
+                                       (rx ".el" eos))
+          (if with-packages
+              (directory-files-recursively package-user-dir
+                                           (rx ".el" eos))
+            nil)))
+
+;;;###autoload
+(defun custom/count-config-lines (&optional with-packages)
+  "Show a buffer with LoC statistics for my Emacs config.
+
+If WITH-PACKAGES is given and non-nil include 3rd party packages
+into the count."
+  (interactive "P")
+  (let ((cloc (executable-find "cloc")))
+    (unless cloc
+      (user-error "Please install cloc"))
+    (with-current-buffer (get-buffer-create " *LoC Emacs configuration*")
+      (text-mode)
+      (read-only-mode)
+      (view-mode)
+      (let ((inhibit-read-only t)
+            (files (custom/all-init-files with-packages)))
+        (erase-buffer)
+        (goto-char (point-min))
+        (apply #'call-process cloc nil t t "--quiet" files))
+      (pop-to-buffer (current-buffer)))))
+
 (provide 'custom-functions)
 
 ;;; custom-functions.el ends here
