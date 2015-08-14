4531563afa352ceb6a57e8a9e69ce0d43045c7bb
Manuel <manuel@boccaperta.com>
2015-08-14 08:15:14 +0200

    Add emacshorrors.com feed

--- lisp/custom-net.el
+++ lisp/custom-net.el
@@ -43,6 +43,7 @@
   :config (progn
             (setq elfeed-feeds
                   '(("http://planet.emacsen.org/atom.xml" emacs)
+                    ("http://emacshorrors.com/feed" emacs)
                     ("http://planet.clojure.in/atom.xml" clojure)
                     ("http://flashstrap.blogspot.com/feeds/posts/default" music)
                     ("http://jazzfromitaly.blogspot.it/feeds/posts/default"
