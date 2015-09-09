b2c3a948447991abae114acdc631f681a327d9fe
Manuel <manuel@boccaperta.com>
2015-09-09 19:57:24 +0200

    Simplify Dired keybindings

--- lisp/custom-files.el
+++ lisp/custom-files.el
@@ -30,90 +30,99 @@
   :bind (("C-c f s"    . dired-get-size)
          ("<C-return>" . custom/open-in-external-app)
          ("C-c f f"    . find-name-dired))
-  :config (progn
-            (setq dired-auto-revert-buffer t            ; Revert buffers on revisiting
-                  dired-listing-switches
-                  "-lFaGh1v --group-directories-first"  ; Add ls switches
-                  global-auto-revert-non-file-buffers t ; Auto refresh dired
-                  auto-revert-verbose nil               ; But be quiet about it
-                  dired-dwim-target t                   ; Use other pane as target
-                  dired-recursive-copies 'always        ; Copy dirs recursively
-                  dired-recursive-deletes ' always      ; Delete dirs recursively
-                  ;; -F marks links with @
-                  dired-ls-F-marks-symlinks t)
+  :config
+  (progn
+    (setq dired-auto-revert-buffer t            ; Revert buffers on revisiting
+          dired-listing-switches
+          "-lFaGh1v --group-directories-first"  ; Add ls switches
+          global-auto-revert-non-file-buffers t ; Auto refresh dired
+          auto-revert-verbose nil               ; But be quiet about it
+          dired-dwim-target t                   ; Use other pane as target
+          dired-recursive-copies 'always        ; Copy dirs recursively
+          dired-recursive-deletes ' always      ; Delete dirs recursively
+          ;; -F marks links with @
+          dired-ls-F-marks-symlinks t)
 
-            ;; Enable dired-find-alternate-file
-            (put 'dired-find-alternate-file 'disabled nil)
+    ;; Enable dired-find-alternate-file
+    (put 'dired-find-alternate-file 'disabled nil)
 
-            ;; 'RET' reuses buffers if they are directories
-            (defun find-file-reuse-dir-buffer ()
-              "Like `dired-find-file', but reuse Dired buffers."
-              (interactive)
-              (set-buffer-modified-p nil)
-              (let ((file (dired-get-file-for-visit)))
-                (if (file-directory-p file)
-                    (find-alternate-file file)
-                  (find-file file))))
+    ;; Reuse buffers if they are directories
+    (defun find-file-reuse-dir-buffer ()
+      "Like `dired-find-file', but reuse Dired buffers."
+      (interactive)
+      (set-buffer-modified-p nil)
+      (let ((file (dired-get-file-for-visit)))
+        (if (file-directory-p file)
+            (find-alternate-file file)
+          (find-file file))))
 
-            (bind-key "RET" #'find-file-reuse-dir-buffer dired-mode-map)
+    ;; Better keybinding for moving between directories
+    (bind-keys :map dired-mode-map
+               ("M-<up>"   . (lambda ()
+                               (interactive)
+                               (find-alternate-file "..")))
+               ("M-p"      . (lambda ()
+                               (interactive)
+                               (find-alternate-file "..")))
+               ("^"        . (lambda ()
+                               (interactive)
+                               (find-alternate-file "..")))
+               ("RET"      . find-file-reuse-dir-buffer)
+               ("M-<down>" . (lambda ()
+                               (interactive)
+                               (dired-find-alternate-file)))
+               ("M-n"      . (lambda ()
+                               (interactive)
+                               (dired-find-alternate-file))))
 
-            ;; '^' reuses the current buffer
-            (defun up-directory ()
-              "Change to parent directory."
-              (interactive)
-              (set-buffer-modified-p nil)
-              (find-alternate-file ".."))
+    ;; Make find-name-dired faster
+    (use-package find-dired
+      :config (setq find-ls-option '("-exec ls -ld {} \\+" . "-ld")))
 
-            (bind-key "^" #'up-directory dired-mode-map)
+    ;; Better M-< and M->
+    (defun dired-back-to-top ()
+      (interactive)
+      (beginning-of-buffer)
+      (dired-next-line 2))
 
-            ;; Make find-name-dired faster
-            (use-package find-dired
-              :config (setq find-ls-option '("-exec ls -ld {} \\+" . "-ld")))
+    (bind-key (vector 'remap 'beginning-of-buffer) 'dired-back-to-top
+              dired-mode-map)
 
-            ;; Better M-< and M->
-            (defun dired-back-to-top ()
-              (interactive)
-              (beginning-of-buffer)
-              (dired-next-line 2))
+    (defun dired-jump-to-bottom ()
+      (interactive)
+      (end-of-buffer)
+      (dired-next-line -1))
 
-            (bind-key (vector 'remap 'beginning-of-buffer) 'dired-back-to-top
-                      dired-mode-map)
+    (bind-key (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom
+              dired-mode-map)
 
-            (defun dired-jump-to-bottom ()
-              (interactive)
-              (end-of-buffer)
-              (dired-next-line -1))
+    ;; Open directory with sudo in dired
+    (defun sudired ()
+      "Open directory with sudo in dired."
+      (interactive)
+      (use-package tramp)
+      (let ((dir (expand-file-name default-directory)))
+        (if (string-match "^/sudo:" dir)
+            (user-error "Already in sudo")
+          (dired (concat "/sudo::" dir)))))
 
-            (bind-key (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom
-                      dired-mode-map)
+    (bind-key "!" #'sudired dired-mode-map)
 
-            ;; Open directory with sudo in dired
-            (defun sudired ()
-              "Open directory with sudo in dired."
-              (interactive)
-              (use-package tramp)
-              (let ((dir (expand-file-name default-directory)))
-                (if (string-match "^/sudo:" dir)
-                    (user-error "Already in sudo")
-                  (dired (concat "/sudo::" dir)))))
+    ;; Get files size in dired
+    (defun dired-get-size ()
+      "Quick and easy way to get file size in dired."
+      (interactive)
+      (let ((files (dired-get-marked-files)))
+        (with-temp-buffer
+          (apply 'call-process "/usr/bin/du" nil t nil "-sch" files)
+          (message
+           "Size of all marked files: %s"
+           (progn
+             (re-search-backward "\\(^[0-9.,]+[A-Za-z]+\\).*total$")
+             (match-string 1))))))
 
-            (bind-key "!" #'sudired dired-mode-map)
-
-            ;; Get files size in dired
-            (defun dired-get-size ()
-              "Quick and easy way to get file size in dired."
-              (interactive)
-              (let ((files (dired-get-marked-files)))
-                (with-temp-buffer
-                  (apply 'call-process "/usr/bin/du" nil t nil "-sch" files)
-                  (message
-                   "Size of all marked files: %s"
-                   (progn
-                     (re-search-backward "\\(^[0-9.,]+[A-Za-z]+\\).*total$")
-                     (match-string 1))))))
-
-            ;; Handle long file names
-            (add-hook 'dired-mode-hook #'toggle-truncate-lines)))
+    ;; Handle long file names
+    (add-hook 'dired-mode-hook #'toggle-truncate-lines)))
 
 (use-package dired-x ; Enable some nice dired features
   :bind ("C-x C-j" . dired-jump)
