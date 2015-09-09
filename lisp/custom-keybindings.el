507d727a61c6c7d15fcd2252306bd6987fd56a36
Manuel <manuel@boccaperta.com>
2015-09-09 14:50:04 +0200

    Add prefix for remote commands

--- lisp/custom-keybindings.el
+++ lisp/custom-keybindings.el
@@ -27,6 +27,7 @@
 (custom/define-group "C-c a a" admin)
 (custom/define-group "C-c a o" org)
 (custom/define-group "C-c a L" language)
+(custom/define-group "C-c a r" remote)
 (custom/define-group "C-c a S" stackexchange)
 (custom/define-group "C-c a w" eww)
 (custom/define-group "C-c b" buffers)
