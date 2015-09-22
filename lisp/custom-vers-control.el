4b78ca092e3d7743d8ecad3e44f3b6f2c4219436
Manuel <manuel@boccaperta.com>
2015-09-22 12:24:52 +0200

    Add nil to make it clearer we do not want it

--- lisp/custom-vers-control.el
+++ lisp/custom-vers-control.el
@@ -71,6 +71,7 @@
 
 (use-package gh ; Github API library
   ;; Don't ensure it, since it's only brought in as dependency
+  :ensure nil
   :defer t
   ;; Change the default profile.  The profile itself is set up via customize,
   ;; and includes auth data, to prevent it from storing tokens in Git config
