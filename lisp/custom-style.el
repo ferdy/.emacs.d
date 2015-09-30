6f9a968beed5eb36cc6d1e2a9bf6a918cc37c6e5
Manuel <manuel@boccaperta.com>
2015-09-30 19:42:28 +0200

    Add keybinding for variable-pitch-mode

--- lisp/custom-style.el
+++ lisp/custom-style.el
@@ -122,6 +122,8 @@ symbols, greek letters, as well as fall backs for."
           x-underline-at-descent-line t)
     (load-theme 'solarized-light 'no-confirm)))
 
+(bind-key "C-c t v" #'variable-pitch-mode)
+
 ;;; Utilities
 (setq custom-safe-themes t) ; Treat themes as safe
 
