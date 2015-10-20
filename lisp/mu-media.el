6715b8ddc5a4fcec2e8f755a5bb406a89df64f9f
Manuel <manuel@boccaperta.com>
2015-10-20 19:27:44 +0200

    Add volume; disable Bongo on the mode line

--- lisp/mu-media.el
+++ lisp/mu-media.el
@@ -17,9 +17,30 @@
   :config (setq camcorder-output-directory "~/videos")
   :diminish (camcorder-mode ". ⓒ"))
 
+(use-package volume ; Control audio volume from Emacs
+  :ensure t)
+
 (use-package bongo ; Play music with Emacs
   :ensure t
-  :bind ("C-c a b" . bongo))
+  :bind (("C-c a b" . bongo)
+         ("C-c f b" . mu-bongo-add-from-dired))
+  ;; Don't use the mode line
+  :init (setq bongo-display-playback-mode-indicator nil
+              bongo-mode-line-indicator-parent nil)
+  :config
+  (progn
+    (defun mu-bongo-add-from-dired ()
+      "Add marked files to Bongo library"
+      (interactive)
+      (let (file-point file (files nil))
+        (dired-map-over-marks
+         (setq file-point (dired-move-to-filename)
+               file (dired-get-filename)
+               files (append files (list file)))
+         nil t)
+        (save-excursion
+          (set-buffer bongo-default-library-buffer-name)
+          (mapc 'bongo-insert-file files))))))
 
 (provide 'mu-media)
 
