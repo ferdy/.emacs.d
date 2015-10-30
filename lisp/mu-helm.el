8994afdb0116895a9107d413212cdd79ad13bf1d
Manuel <manuel@boccaperta.com>
2015-10-30 09:17:37 +0100

    Set helm-recentf-fuzzy match using Customize UI
    Reason: https://emacs.stackexchange.com/questions/102/advantages-of-setting-variables-with-setq-instead-of-custom-el/106#106

--- lisp/mu-helm.el
+++ lisp/mu-helm.el
@@ -75,17 +75,10 @@
           helm-ff-skip-boring-files t
           ;; Search for library in `require' and `declare-function' sexp
           helm-ff-search-library-in-sexp t
-          ;; Fuzzy matching
-          ;; helm-recentf-fuzzy-match t
           ;; Auto-complete in find-files
           helm-ff-auto-update-initial-value t
           ;; Sort directories first
-          helm-find-files-sort-directories t)
-
-    ;; helm-recentf-fuzzy-match var is broken: redeclare it manually
-    (setq helm-source-recentf
-          (helm-make-source "Recentf" 'helm-recentf-source
-            :fuzzy-match t))))
+          helm-find-files-sort-directories t)))
 
 (use-package helm-misc ; Misc Helm commands
   :ensure helm
