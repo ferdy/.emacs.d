984fa5e71025cad24490c7644db808e5a278ad2d
Manuel <manuel@boccaperta.com>
2015-11-09 17:10:10 +0100

    Drop zenburn

--- lisp/mu-style.el
+++ lisp/mu-style.el
@@ -118,10 +118,6 @@ symbols, greek letters, as well as fall backs for."
 
     (load-theme 'solarized-light 'no-confirm)))
 
-(use-package zenburn ; Beautiful dark theme
-  :ensure zenburn-theme
-  :defer t)
-
 (use-package darktooth ; Another beautiful dark theme
   :ensure darktooth-theme
   :defer t)
