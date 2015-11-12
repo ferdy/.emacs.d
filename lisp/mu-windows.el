ea1ca9d0e32ceb379bd2f7cbb2e9cf770ff2561b
Manuel <manuel@boccaperta.com>
2015-11-12 16:38:27 +0100

    Add window split toggle
    See: https://emacs.stackexchange.com/a/5372/5514

--- lisp/mu-windows.el
+++ lisp/mu-windows.el
@@ -78,6 +78,23 @@
  ("C-S-<left>"  . shrink-window-horizontally)
  ("C-S-<right>" . enlarge-window-horizontally))
 
+;;;###autoload
+(defun mu-window-split-toggle ()
+  "Toggle between horizontal and vertical split with two windows."
+  (interactive)
+  (if (> (length (window-list)) 2)
+      (error "Can't toggle with more than 2 windows!")
+    (let ((func (if (window-full-height-p)
+                    #'split-window-vertically
+                  #'split-window-horizontally)))
+      (delete-other-windows)
+      (funcall func)
+      (save-selected-window
+        (other-window 1)
+        (switch-to-buffer (other-buffer))))))
+
+(bind-key "C-c w t" #'mu-window-split-toggle)
+
 (provide 'mu-windows)
 
 ;;; mu-windows.el ends here
