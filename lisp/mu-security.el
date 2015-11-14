f1f94b16e016a6ecd4cf460d9847f2258d2d3311
Manuel <manuel@boccaperta.com>
2015-11-14 12:12:24 +0100

    Use ca-certificates instead of certifi

--- lisp/mu-security.el
+++ lisp/mu-security.el
@@ -12,20 +12,9 @@
 ;;; Code:
 
 ;; Verify secure connections
-(defun mu-certificate-bundle ()
-  "Get the path to a certificate bundle."
-  (let* ((lines (or (ignore-errors (process-lines "python3" "-m" "certifi"))
-                    (ignore-errors (process-lines "python2" "-m" "certifi"))
-                    (ignore-errors (process-lines "python" "-m" "certifi"))))
-         (cert-bundle (car lines)))
-    (unless (file-exists-p cert-bundle)
-      (error "Failed to find a certificate bundle.  Install certifi"))
-    cert-bundle))
-
 (setq gnutls-verify-error t)
 
-(if (gnutls-available-p)
-    (setq gnutls-trustfiles (mu-certificate-bundle))
+(unless (gnutls-available-p)
   (run-with-idle-timer
    2 nil
    (lambda ()
