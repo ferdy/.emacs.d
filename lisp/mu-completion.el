997b2abaa92f0e5aa57c5962ac4e39f4793606ef
Manuel <manuel@boccaperta.com>
2015-11-15 08:12:11 +0100

    Add company-emojify
    See also: https://github.com/dunn/company-emoji/issues/5#issuecomment-156746277

--- lisp/mu-completion.el
+++ lisp/mu-completion.el
@@ -103,6 +103,14 @@
   :after company
   :init (add-to-list 'company-backends 'company-restclient))
 
+(use-package company-emoji ; Backend for Emoji
+  :ensure t
+  :after company
+  :init (add-to-list 'company-backends 'company-emoji)
+  :config
+  ;; Use emojify
+  (setq company-emoji-insert-unicode nil))
+
 (provide 'mu-completion)
 
 ;;; mu-completion.el ends here
