d5e8527a544331b905b13ce866b2cc58b9b415ec
Manuel <manuel@boccaperta.com>
2015-12-06 08:50:32 +0100

    Add narrow-or-widen-dwim
    See: http://endlessparentheses.com/emacs-narrow-or-widen-dwim.html

--- lisp/mu-editing.el
+++ lisp/mu-editing.el
@@ -404,6 +404,37 @@ Including indent-buffer, which should not be called automatically on save."
 
 (bind-key "C-c t f" #'mu-fill-buffer)
 
+;;;###autoload
+(defun narrow-or-widen-dwim (p)
+  "Widen if buffer is narrowed, narrow-dwim otherwise.
+Dwim means: region, org-src-block, org-subtree, or defun,
+whichever applies first. Narrowing to org-src-block actually
+calls `org-edit-src-code'.
+
+With prefix P, don't widen, just narrow even if buffer is
+already narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning) (region-end)))
+        ((derived-mode-p 'org-mode)
+         ;; `org-edit-src-code' is not a real narrowing
+         ;; command. Remove this first conditional if you
+         ;; don't want it.
+         (cond ((ignore-errors (org-edit-src-code))
+                (delete-other-windows))
+               ((ignore-errors (org-narrow-to-block) t))
+               (t (org-narrow-to-subtree))))
+        ((derived-mode-p 'latex-mode)
+         (LaTeX-narrow-to-environment))
+        (t (narrow-to-defun))))
+
+(bind-key "C-x n" #'narrow-or-widen-dwim)
+
+(eval-after-load 'latex
+  '(bind-key "C-x n" nil LaTeX-mode-map))
+
 (provide 'mu-editing)
 
 ;;; mu-editing.el ends here
