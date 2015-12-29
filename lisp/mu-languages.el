8087d51ec8800924ae83474257333b5faed872f9
manuel-uberti <manuel@boccaperta.com>
2015-12-29 15:48:07 +0100

    Translate text IT>EN using WordReference

--- lisp/mu-languages.el
+++ lisp/mu-languages.el
@@ -122,6 +122,21 @@ With prefix P, create local abbrev. Otherwise it will be global."
   :bind ("C-c a L g" . writegood-mode)
   :diminish writegood-mode)
 
+;;; Utilities and keybindings
+;;;###autoload
+(defun custom-wordreference ()
+  "Translate text from Italian to English with WordReference.
+Check the selected region if any or display a query prompt otherwise."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.wordreference.com/iten/"
+    (url-hexify-string (if mark-active
+                           (buffer-substring (region-beginning) (region-end))
+                         (read-string "WordReference: "))))))
+
+(bind-key "C-c a L t" #'custom-wordreference)
+
 (provide 'mu-languages)
 
 ;;; mu-languages.el ends here
