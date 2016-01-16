4daf23c01418f55c22a2e50c04858e9f107b0df4
manuel-uberti <manuel@boccaperta.com>
2016-01-16 09:34:41 +0100

    Add zerodark theme

--- lisp/mu-style.el
+++ lisp/mu-style.el
@@ -168,6 +168,9 @@ symbols, greek letters, as well as fall backs for."
 
     (load-theme 'solarized-light 'no-confirm)))
 
+(use-package zerodark                   ; Beautiful dark theme
+  :ensure zerodark-theme)
+
 ;;; Mode line
 (use-package smart-mode-line            ; Better mode-line
   :ensure t
