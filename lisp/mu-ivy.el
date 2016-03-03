94e37c66cf5eb4a70ec337f676275cff2dfb7423
manuel-uberti <manuel@boccaperta.com>
2016-03-03 09:46:41 +0100

    Use counsel-mode

--- lisp/mu-ivy.el
+++ lisp/mu-ivy.el
@@ -37,18 +37,14 @@
 
 (use-package counsel                    ; Completion functions with Ivy
   :ensure t
-  :bind (("C-x C-f" . counsel-find-file)
-         ("M-x"     . counsel-M-x)
-         ("C-h v"   . counsel-describe-variable)
-         ("C-h f"   . counsel-describe-function)
-         ("C-h S"   . counsel-info-lookup-symbol)
+  :init (counsel-mode 1)
+  :bind (("C-h S"   . counsel-info-lookup-symbol)
          ("C-c u"   . counsel-unicode-char)
          ("C-c g"   . counsel-git)
          ("C-c G"   . counsel-git-grep)
          ("C-c k"   . counsel-ag)
          ("C-x l"   . counsel-locate)
-         ("C-x i"   . counsel-imenu)
-         ("M-y"     . counsel-yank-pop))
+         ("C-x i"   . counsel-imenu))
   :config
   (progn
     (setq counsel-find-file-at-point t
@@ -63,7 +59,8 @@
      'counsel-find-file
      `(("x"
         (lambda (x) (delete-file (expand-file-name x ivy--directory)))
-        ,(propertize "delete" 'face 'font-lock-warning-face))))))
+        ,(propertize "delete" 'face 'font-lock-warning-face)))))
+  :diminish counsel-mode)
 
 (provide 'mu-ivy)
 
