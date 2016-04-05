589609d759af6b00d8b9104e5e196533f5008166
manuel-uberti <manuel@boccaperta.com>
2016-04-05 14:52:03 +0200

    Remove progn; better style

--- lisp/mu-latex.el
+++ lisp/mu-latex.el
@@ -19,42 +19,41 @@
   :ensure auctex
   :defer t
   :config
-  (progn
-    (setq TeX-clean-confirm nil         ; No confirmation when cleaning
-          ;; Parse documents to provide completion
-          TeX-parse-self t
-          ;; Automatically save style information
-          TeX-auto-save t
-          ;; Automatically save when compiling
-          TeX-save-query nil
-          ;; Insert braces after sub- and superscripts in math mode
-          TeX-electric-sub-and-superscript t
-          ;; Don't insert magic quotes right away
-          TeX-quote-after-quote t
-          ;; Provide forward and inverse search with SyncTeX
-          TeX-source-correlate-mode t
-          TeX-source-correlate-method 'synctex
-          ;; Tune fill-paragraph
-          LaTeX-fill-break-at-separators '(\\\( \\\[))
+  (setq TeX-clean-confirm nil         ; No confirmation when cleaning
+        ;; Parse documents to provide completion
+        TeX-parse-self t
+        ;; Automatically save style information
+        TeX-auto-save t
+        ;; Automatically save when compiling
+        TeX-save-query nil
+        ;; Insert braces after sub- and superscripts in math mode
+        TeX-electric-sub-and-superscript t
+        ;; Don't insert magic quotes right away
+        TeX-quote-after-quote t
+        ;; Provide forward and inverse search with SyncTeX
+        TeX-source-correlate-mode t
+        TeX-source-correlate-method 'synctex
+        ;; Tune fill-paragraph
+        LaTeX-fill-break-at-separators '(\\\( \\\[))
 
-    (setq-default TeX-master nil        ; Ask for the master file
-                  TeX-engine 'luatex    ; Use luatex
-                  )
+  (setq-default TeX-master nil        ; Ask for the master file
+                TeX-engine 'luatex    ; Use luatex
+                )
 
-    ;; Move to chktex
-    (setcar (cdr (assoc "Check" TeX-command-list)) "chktex -v6 %s")
+  ;; Move to chktex
+  (setcar (cdr (assoc "Check" TeX-command-list)) "chktex -v6 %s")
 
-    ;; Tell Emacs how to parse TeX files
-    (add-hook 'tex-mode-hook
-              #'(lambda () (setq ispell-parser 'tex)))
+  ;; Tell Emacs how to parse TeX files
+  (add-hook 'tex-mode-hook
+            #'(lambda () (setq ispell-parser 'tex)))
 
-    ;; Use pdf-tools to open PDF files
-    (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
-          TeX-source-correlate-start-server t)
+  ;; Use pdf-tools to open PDF files
+  (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+        TeX-source-correlate-start-server t)
 
-    ;; Update PDF buffers after successful LaTeX runs
-    (add-hook 'TeX-after-TeX-LaTeX-command-finished-hook
-              #'TeX-revert-document-buffer)))
+  ;; Update PDF buffers after successful LaTeX runs
+  (add-hook 'TeX-after-TeX-LaTeX-command-finished-hook
+            #'TeX-revert-document-buffer))
 
 (use-package tex-buf                    ; External commands for AUCTeX
   :ensure auctex
@@ -65,9 +64,10 @@
 (use-package tex-style           ; Customizable variables for AUCTeX style files
   :ensure auctex
   :defer t
+  :config
   ;; Enable support for csquotes
-  :config (setq LaTeX-csquotes-close-quote "}"
-                LaTeX-csquotes-open-quote "\\enquote{"))
+  (setq LaTeX-csquotes-close-quote "}"
+        LaTeX-csquotes-open-quote "\\enquote{"))
 
 (use-package tex-fold                   ; Fold TeX macros
   :ensure auctex
@@ -77,28 +77,29 @@
 (use-package tex-mode                   ; Major mode for TeX files
   :ensure auctex
   :defer t
-  :config (font-lock-add-keywords 'latex-mode
-                                  `((,(rx "\\"
-                                          symbol-start
-                                          "fx" (1+ (or (syntax word)
-                                                       (syntax symbol)))
-                                          symbol-end)
-                                     . font-lock-warning-face))))
+  :config
+  (font-lock-add-keywords 'latex-mode
+                          `((,(rx "\\"
+                                  symbol-start
+                                  "fx" (1+ (or (syntax word)
+                                               (syntax symbol)))
+                                  symbol-end)
+                             . font-lock-warning-face))))
 
 (use-package latex                      ; Support for LaTeX documents
   :ensure auctex
   :defer t
   :config
-  (progn
-    (setq TeX-outline-extra `((,(rx (0+ space) "\\section*{") 2)
-                              (,(rx (0+ space) "\\subsection*{") 3)
-                              (,(rx (0+ space) "\\subsubsection*{") 4)
-                              (,(rx (0+ space) "\\minisec{") 5))
-          LaTeX-babel-hyphen nil        ; No language-specific hyphens please
-          LaTeX-command-style
-          '(("" "%(PDF)%(latex) -file-line-error %S%(PDFout)")))
+  (setq TeX-outline-extra `((,(rx (0+ space) "\\section*{") 2)
+                            (,(rx (0+ space) "\\subsection*{") 3)
+                            (,(rx (0+ space) "\\subsubsection*{") 4)
+                            (,(rx (0+ space) "\\minisec{") 5))
+        LaTeX-babel-hyphen nil        ; No language-specific hyphens please
+        LaTeX-command-style
+        '(("" "%(PDF)%(latex) -file-line-error %S%(PDFout)")))
 
-    (add-hook 'LaTeX-mode-hook #'LaTeX-math-mode))) ; Easy math input
+  ;; Easy math input
+  (add-hook 'LaTeX-mode-hook #'LaTeX-math-mode))
 
 (use-package latex-extra                ; Useful functionalities to LaTeX-mode
   :ensure t
@@ -114,35 +115,32 @@
 (use-package bibtex                     ; Edit and validate BibTeX files
   :defer t
   :config
-  (progn
-    ;; Run prog mode hooks for bibtex
-    (add-hook 'bibtex-mode-hook (lambda () (run-hooks 'prog-mode-hook)))
-    ;; Use a modern BibTeX dialect
-    (bibtex-set-dialect 'biblatex)))
+  ;; Run prog mode hooks for bibtex
+  (add-hook 'bibtex-mode-hook (lambda () (run-hooks 'prog-mode-hook)))
+  ;; Use a modern BibTeX dialect
+  (bibtex-set-dialect 'biblatex))
 
 (use-package reftex        ; Minor mode for \label, \ref, \cite, \index in LaTeX
   :defer t
   :init (add-hook 'LaTeX-mode-hook #'reftex-mode)
   :config
-  (progn
-    ;; Plug into AUCTeX
-    (setq reftex-plug-into-AUCTeX t
-          ;; Automatically derive labels, and prompt for confirmation
-          reftex-insert-label-flags '(t t))
+  (setq reftex-plug-into-AUCTeX t
+        ;; Automatically derive labels, and prompt for confirmation
+        reftex-insert-label-flags '(t t))   ;; Plug into AUCTeX
 
-    ;; Provide basic RefTeX support for biblatex
-    (unless (assq 'biblatex reftex-cite-format-builtin)
-      (add-to-list 'reftex-cite-format-builtin
-                   '(biblatex "The biblatex package"
-                              ((?\C-m . "\\cite[]{%l}")
-                               (?t    . "\\textcite{%l}")
-                               (?a    . "\\autocite[]{%l}")
-                               (?p    . "\\parencite{%l}")
-                               (?f    . "\\footcite[][]{%l}")
-                               (?F    . "\\fullcite[]{%l}")
-                               (?x    . "[]{%l}")
-                               (?X    . "{%l}"))))
-      (setq reftex-cite-format 'biblatex)))
+  ;; Provide basic RefTeX support for biblatex
+  (unless (assq 'biblatex reftex-cite-format-builtin)
+    (add-to-list 'reftex-cite-format-builtin
+                 '(biblatex "The biblatex package"
+                            ((?\C-m . "\\cite[]{%l}")
+                             (?t    . "\\textcite{%l}")
+                             (?a    . "\\autocite[]{%l}")
+                             (?p    . "\\parencite{%l}")
+                             (?f    . "\\footcite[][]{%l}")
+                             (?F    . "\\fullcite[]{%l}")
+                             (?x    . "[]{%l}")
+                             (?X    . "{%l}"))))
+    (setq reftex-cite-format 'biblatex))
   :diminish reftex-mode)
 
 (use-package latex-unicode-math-mode    ; Input method for Unicode math symbols
--- lisp/mu-vers-control.el
+++ lisp/mu-vers-control.el
@@ -25,77 +25,76 @@
          ("C-c v g" . magit-blame)
          ("C-c v l" . magit-log-buffer-file)
          ("C-c v p" . magit-pull))
-  ;; Aggressively commit to WIP refs on any change
   :init
-  (progn
-    (magit-wip-after-save-mode)
-    (magit-wip-after-apply-mode)
-    (magit-wip-before-change-mode))
+  ;; Aggressively commit to WIP refs on any change
+  (magit-wip-after-save-mode)
+  (magit-wip-after-apply-mode)
+  (magit-wip-before-change-mode)
   :config
-  (progn
-    ;; Be quiet
-    (setq magit-save-repository-buffers 'dontask
-          magit-refs-show-commit-count 'all
-          magit-push-always-verify nil
-          magit-revision-show-gravatars nil)
+  ;; Be quiet
+  (setq magit-save-repository-buffers 'dontask
+        magit-refs-show-commit-count 'all
+        magit-push-always-verify nil
+        magit-revision-show-gravatars nil)
 
-    ;; Use Ivy
-    (setq magit-completing-read-function 'ivy-completing-read)
+  ;; Use Ivy
+  (setq magit-completing-read-function 'ivy-completing-read)
 
-    ;; Set `magit-status' fullscreen
-    (setq magit-post-display-buffer-hook
-          #'(lambda ()
-              (when (derived-mode-p 'magit-status-mode)
-                (delete-other-windows))))
+  ;; Set `magit-status' fullscreen
+  (setq magit-post-display-buffer-hook
+        #'(lambda ()
+            (when (derived-mode-p 'magit-status-mode)
+              (delete-other-windows))))
 
-    ;; Kill Magit buffers when quitting `magit-status'
-    (defun magit-quit-session (&optional kill-buffer)
-      "Kill all Magit buffers on quit"
-      (interactive)
-      (magit-restore-window-configuration kill-buffer)
-      (mu-kill-buffers "^\\*magit"))
+  ;; Kill Magit buffers when quitting `magit-status'
+  (defun magit-quit-session (&optional kill-buffer)
+    "Kill all Magit buffers on quit"
+    (interactive)
+    (magit-restore-window-configuration kill-buffer)
+    (mu-kill-buffers "^\\*magit"))
 
-    (bind-key "q" #'magit-quit-session magit-status-mode-map)
+  (bind-key "q" #'magit-quit-session magit-status-mode-map)
 
-    ;; Set `magit-repository-directories' for `magit-status'
-    (defun mu-magit-set-repo-dirs-from-projectile ()
-      "Set `magit-repository-directories' with known Projectile projects."
-      (let ((project-dirs (bound-and-true-p projectile-known-projects)))
-        ;; Remove trailing slashes from project directories
-        (setq magit-repository-directories
-              (mapcar #'directory-file-name project-dirs))))
+  ;; Set `magit-repository-directories' for `magit-status'
+  (defun mu-magit-set-repo-dirs-from-projectile ()
+    "Set `magit-repository-directories' with known Projectile projects."
+    (let ((project-dirs (bound-and-true-p projectile-known-projects)))
+      ;; Remove trailing slashes from project directories
+      (setq magit-repository-directories
+            (mapcar #'directory-file-name project-dirs))))
 
-    (with-eval-after-load 'projectile
-      (mu-magit-set-repo-dirs-from-projectile))
+  (with-eval-after-load 'projectile
+    (mu-magit-set-repo-dirs-from-projectile))
 
-    (add-hook 'projectile-switch-project-hook
-              #'mu-magit-set-repo-dirs-from-projectile)
+  (add-hook 'projectile-switch-project-hook
+            #'mu-magit-set-repo-dirs-from-projectile)
 
-    ;; Refresh `diff-hl' accordingly
-    (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
-    ;; Refresh `magit-status' after saving a buffer
-    (add-hook 'after-save-hook 'magit-after-save-refresh-status))
+  ;; Refresh `diff-hl' accordingly
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+
+  ;; Refresh `magit-status' after saving a buffer
+  (add-hook 'after-save-hook 'magit-after-save-refresh-status)
   :diminish (magit-wip-after-save-local-mode
              magit-wip-before-change-mode))
 
 (use-package magit-rockstar             ; Extra functions for Magit
   :ensure t
   :init
-  (progn
-    (magit-define-popup-action 'magit-rebase-popup
-      ?R "Rockstar" 'magit-rockstar)
-    (magit-define-popup-action 'magit-commit-popup
-      ?n "Reshelve" 'magit-reshelve)
-    (magit-define-popup-action 'magit-branch-popup
-      ?R "Toggle rebasing" 'magit-branch-toggle-rebase)
-    (magit-define-popup-action 'magit-fetch-popup
-      ?p "Pull request" 'magit-branch-pull-request)))
+  (magit-define-popup-action 'magit-rebase-popup
+    ?R "Rockstar" 'magit-rockstar)
+  (magit-define-popup-action 'magit-commit-popup
+    ?n "Reshelve" 'magit-reshelve)
+  (magit-define-popup-action 'magit-branch-popup
+    ?R "Toggle rebasing" 'magit-branch-toggle-rebase)
+  (magit-define-popup-action 'magit-fetch-popup
+    ?p "Pull request" 'magit-branch-pull-request))
 
 (use-package git-commit                 ; Git commit message mode
   :ensure t
   :init (global-git-commit-mode)
-  :config (remove-hook 'git-commit-finish-query-functions
-                       #'git-commit-check-style-conventions))
+  :config
+  (remove-hook 'git-commit-finish-query-functions
+               #'git-commit-check-style-conventions))
 
 (use-package gitconfig-mode             ; Git configuration mode
   :ensure t
