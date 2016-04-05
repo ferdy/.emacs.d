c318cc62af50657dc9de8b941998306dff221409
manuel-uberti <manuel@boccaperta.com>
2016-04-05 20:00:18 +0200

    Add handy functions
    See: https://github.com/lunaryorn/.emacs.d/blob/master/lisp/lunaryorn-simple.el

--- lisp/mu-editing.el
+++ lisp/mu-editing.el
@@ -441,6 +441,80 @@ already narrowed."
 (with-eval-after-load 'latex
   (bind-key "C-x n" nil LaTeX-mode-map))
 
+
+;;;###autoload
+(defun mu-smart-kill-whole-line (&optional arg)
+  "Kill whole line and move back to indentation.
+Kill the whole line with function `kill-whole-line' and then move
+`back-to-indentation'."
+  (interactive "p")
+  (kill-whole-line arg)
+  (back-to-indentation))
+
+;;;###autoload
+(defun mu-smart-backward-kill-line ()
+  "Kill line backwards and re-indent."
+  (interactive)
+  (kill-line 0)
+  (indent-according-to-mode))
+
+;;;###autoload
+(defun mu-smart-open-line ()
+  "Insert empty line after the current line."
+  (interactive)
+  (move-end-of-line nil)
+  (newline-and-indent))
+
+;;;###autoload
+(defun mu-back-to-indentation-or-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;;;###autoload
+(define-minor-mode mu-auto-fill-comments-mode
+  "Minor mode to auto-fill comments only."
+  :lighter nil
+  :keymap nil
+  (cond
+   (mu-auto-fill-comments-mode
+    (setq-local comment-auto-fill-only-comments t)
+    (auto-fill-mode 1))
+   (:else
+    (kill-local-variable 'comment-auto-fill-only-comments)
+    (auto-fill-mode -1))))
+
+;;;###autoload
+(defun mu-insert-current-date (iso)
+  "Insert the current date at point.
+When ISO is non-nil, insert the date in ISO 8601 format.
+Otherwise insert the date as Mar 04, 2014."
+  (interactive "P")
+  (insert (format-time-string (if iso "%F" "%b %d, %Y"))))
+
+(bind-keys
+ ([remap kill-whole-line]        . mu-smart-kill-whole-line)
+ ([remap move-beginning-of-line] . mu-back-to-indentation-or-beginning-of-line)
+ ("C-<backspace>"                . mu-smart-backward-kill-line)
+ ("C-S-j"                        . mu-smart-open-line)
+ ("C-c i d"                      . mu-insert-current-date))
+
 (provide 'mu-editing)
 
 ;;; mu-editing.el ends here
