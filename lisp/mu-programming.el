e0870b7c087016b222863218ea687299e5c24832
Manuel Uberti <manuel@boccaperta.com>
2016-04-13 15:19:35 +0200

    Better binding for clj-refactor

--- lisp/mu-programming.el
+++ lisp/mu-programming.el
@@ -165,7 +165,7 @@
   (defun mu-clojure-mode-hook ()
     (clj-refactor-mode 1)
     (yas-minor-mode 1)                ; For adding require/use/import
-    (cljr-add-keybindings-with-prefix "C-c m r"))
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
 
   (add-hook 'clojure-mode-hook #'mu-clojure-mode-hook)
   :config
