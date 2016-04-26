af78bfdd546f1c8682e0307fe429719655dde06b
Manuel Uberti <manuel@boccaperta.com>
2016-04-26 10:08:21 +0200

    Sort use-package declarations based on dependency

--- lisp/mu-ivy.el
+++ lisp/mu-ivy.el
@@ -38,6 +38,13 @@
                       (sexp (bookmark-jump "ba-server"))))))
   :diminish ivy-mode)
 
+(use-package swiper                     ; Isearch with an overview
+  :ensure t
+  :bind (("C-c s s" . counsel-grep-or-swiper)
+         ("C-c s S" . swiper-all))
+  :bind  (:map isearch-mode-map
+               ("M-i" . swiper-from-isearch)))
+
 (use-package smex                       ; Better M-x interface
   :ensure t)
 
@@ -65,13 +72,6 @@
          ;; file names ending with # or ~
          "\\|\\(?:\\`.+?[#~]\\'\\)"))
   :diminish counsel-mode)
-
-(use-package swiper                     ; Isearch with an overview
-  :ensure t
-  :bind (("C-c s s" . counsel-grep-or-swiper)
-         ("C-c s S" . swiper-all))
-  :bind  (:map isearch-mode-map
-               ("M-i" . swiper-from-isearch)))
 
 (provide 'mu-ivy)
 
