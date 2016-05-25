7625a2abb932361484fffaa52d0f72b8da65e919
Manuel Uberti <manuel@boccaperta.com>
2016-05-25 13:08:49 +0200

    Simplify company-mode setup

--- lisp/mu-completion.el
+++ lisp/mu-completion.el
@@ -83,15 +83,6 @@ _e_: extra   _l_: list        _n_: new
         company-tooltip-flip-when-above t
         ;; Easy navigation to candidates with M-<n>
         company-show-numbers t)
-
-  ;; Don't complete numbers
-  (push (apply-partially #'cl-remove-if
-                         (lambda (c)
-                           (or (string-match-p "[^\x00-\x7F]+" c)
-                               (string-match-p "[0-9]+" c)
-                               (if (equal major-mode "org")
-                                   (>= (length c) 15)))))
-        company-transformers)
   :diminish company-mode)
 
 (use-package company-statistics         ; Show likelier candidates on top
