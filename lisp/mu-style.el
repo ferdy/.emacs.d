b089c4f20eb2075c4cb55d11a497642ed07573a9
Manuel Uberti <manuel.uberti@inventati.org>
2016-08-30 21:22:49 +0200

    Fix mode line for long lines

--- lisp/mu-style.el
+++ lisp/mu-style.el
@@ -65,6 +65,9 @@ as well as fall backs for."
 
 (setq echo-keystrokes 0.1)              ; Faster echo keystrokes
 
+;; Avoid showing ?? in the mode line when we have long lines.
+(setq line-number-display-limit-width 10000)
+
 ;; Turn off mouse interface early in startup to avoid momentary display
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
