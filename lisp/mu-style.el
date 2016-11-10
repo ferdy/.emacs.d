eb11730fa0bc5f88f450a17d5b7c1ab9f542f3ec
Manuel Uberti <manuel.uberti@inventati.org>
2016-11-10 06:26:49 +0100

    Switch back to default mode-line
    Less clutter.

--- lisp/mu-style.el
+++ lisp/mu-style.el
@@ -164,49 +164,6 @@ as well as fall backs for."
 (line-number-mode)
 (column-number-mode)
 
-(use-package which-func                 ; Current function name
-  :defer 1
-  :config
-  (which-function-mode)
-  (validate-setq
-   which-func-unknown "⊥"               ; The default is really boring…
-   which-func-format
-   `((:propertize (" ➤ " which-func-current)
-                  local-map ,which-func-keymap
-                  face which-func
-                  mouse-face mode-line-highlight
-                  help-echo "mouse-1: go to beginning\n\
-mouse-2: toggle rest visibility\n\
-mouse-3: go to end"))))
-
-(use-package spaceline-config           ; A beautiful mode line
-  :ensure spaceline
-  :config
-  (spaceline-compile
-   'mu
-   ;; Left side of the mode line (all the important stuff)
-   '(((buffer-modified buffer-size input-method) :face highlight-face)
-     '(buffer-id remote-host buffer-encoding-abbrev)
-     ((point-position line-column buffer-position selection-info)
-      :separator " | ")
-     major-mode
-     process
-     (flycheck-error flycheck-warning flycheck-info)
-     (python-pyvenv :fallback python-pyenv)
-     ((which-function projectile-root) :separator " @ ")
-     ((minor-modes :separator spaceline-minor-modes-separator) :when active))
-   ;; Right segment (the unimportant stuff)
-   '((version-control :when active)))
-
-  (setq-default mode-line-format '("%e" (:eval (spaceline-ml-mu)))))
-
-(use-package powerline                  ; The work-horse of Spaceline
-  :ensure t
-  :after spaceline-config
-  :config (validate-setq
-           powerline-height (truncate (* 1.0 (frame-char-height)))
-           powerline-default-separator 'utf-8))
-
 (provide 'mu-style)
 
 ;; Local Variables:
