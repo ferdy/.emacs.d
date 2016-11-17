da9ec0383c3ae7a707d8657e4d17720f73e40b70
Manuel Uberti <manuel.uberti@inventati.org>
2016-11-17 22:03:01 +0100

    Split windows sensibly

--- lisp/mu-editing.el
+++ lisp/mu-editing.el
@@ -80,8 +80,14 @@
                   text-mode-hook))
     (add-hook hook #'visual-fill-column-mode))
   ;; Centre text by default, and move the fringes close to the text.
-  :config (setq-default visual-fill-column-center-text t
-                        visual-fill-column-fringes-outside-margins nil))
+  :config
+  (setq-default visual-fill-column-center-text t
+                visual-fill-column-fringes-outside-margins nil)
+
+  ;; Split windows vertically despite large margins, because Emacs otherwise
+  ;; refuses to vertically split windows with large margins
+  (validate-setq split-window-preferred-function
+                 #'visual-fill-column-split-window-sensibly))
 
 (use-package align                      ; Align text in buffers
   :bind (("C-c x a a" . align)
