2a0ed9074d678f8623d056648cd2855eb15f16ce
Manuel Uberti <manuel.uberti@inventati.org>
2017-05-04 11:58:05 +0200

    Remove whitespaces

--- lisp/mu-vers-control.el
+++ lisp/mu-vers-control.el
@@ -21,7 +21,7 @@
   :ensure t
   :bind (("C-c v c" . magit-clone)
          ("C-c v C" . magit-checkout)
-         ("C-c v d" . magit-dispatch-popup)         
+         ("C-c v d" . magit-dispatch-popup)
          ("C-c v g" . magit-blame)
          ("C-c v l" . magit-log-buffer-file)
          ("C-c v p" . magit-pull)
