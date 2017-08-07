ac3ada43f6d0ed6556cf56644dda9d9e78f23e6c
Manuel Uberti <manuel.uberti@inventati.org>
2017-08-07 15:41:07 +0200

    Customize mode line

--- lisp/mu-style.el
+++ lisp/mu-style.el
@@ -150,9 +150,6 @@
 (line-number-mode)
 (column-number-mode)
 
-;; Show buffer position percentage starting from top
-(validate-setq mode-line-percent-position '(-3 "%o"))
-
 ;; Increase mode-line size with a border (box) of the same colour and
 ;; reduce font size by tweaking height
 (set-face-attribute 'mode-line nil
@@ -163,6 +160,45 @@
                     :inverse-video nil
                     :height 0.9
                     :box '(:line-width 6 :color "#fdf6e3" :style nil))
+
+;; Show buffer position percentage starting from top
+(validate-setq mode-line-percent-position '(-3 "%o"))
+
+(defvar mu-projectile-mode-line
+  '(:propertize
+    (:eval (when (ignore-errors (projectile-project-root))
+             (concat " " (projectile-project-name))))
+    face font-lock-constant-face)
+  "Mode line format for Projectile.")
+(put 'mu-projectile-mode-line 'risky-local-variable t)
+
+(defvar mu-vc-mode-line
+  '(" " (:propertize
+         ;; Strip the backend name from the VC status information
+         (:eval (let ((backend (symbol-name (vc-backend (buffer-file-name)))))
+                  (substring vc-mode (+ (length backend) 2))))
+         face font-lock-variable-name-face))
+  "Mode line format for VC Mode.")
+(put 'mu-vc-mode-line 'risky-local-variable t)
+
+(setq-default mode-line-format
+              '("%e" mode-line-front-space
+                ;; Standard info about the current buffer
+                mode-line-mule-info
+                mode-line-client
+                mode-line-modified
+                mode-line-remote
+                mode-line-frame-identification
+                mode-line-buffer-identification " " mode-line-position
+                ;; Some specific information about the current buffer:
+                mu-projectile-mode-line ; Project information
+                (vc-mode mu-vc-mode-line) ; VC information
+                (multiple-cursors-mode mc/mode-line) ; Number of cursors
+                ;; Misc information, notably battery state and function name
+                " "
+                mode-line-misc-info
+                ;; And the modes, which I don't really care for anyway
+                " " mode-line-modes mode-line-end-spaces))
 
 ;;; Utilities and key bindings
 (defun mu-reset-fonts ()
