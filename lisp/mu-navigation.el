6b64a467e10cbedad9e81af85c3cd7437643ee2d
Manuel Uberti <manuel.uberti@inventati.org>
2017-08-19 09:02:23 +0200

    Add dumb-jump

--- lisp/mu-navigation.el
+++ lisp/mu-navigation.el
@@ -62,6 +62,16 @@
              beginend-prog-mode
              beginend-vc-dir-mode))
 
+(use-package dumb-jump                  ; Jump to definitions
+  :ensure t
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config
+  (validate-setq dumb-jump-selector 'ivy))
+
 ;; Quickly pop the mark several times with C-u C-SPC C-SPC
 (validate-setq set-mark-command-repeat-pop t)
 
