188ac355dce5aac6dee562a7839400d899727588
Manuel Uberti <manuel.uberti@inventati.org>
2017-12-08 09:45:20 +0100

    Revert to default ivy candidates sorting

--- lisp/mu-ivy.el
+++ lisp/mu-ivy.el
@@ -32,9 +32,7 @@
    ;; Always ignore buffers set in `ivy-ignore-buffers'
    ivy-use-ignore-default 'always
    ;; Ignore some buffers in `ivy-switch-buffer'
-   ivy-ignore-buffers '("company-statistics-cache.el" ".elfeed/index")
-   ;; Sort candidates like `ido-switch-buffer'
-   ivy-sort-matches-functions-alist '((t . nil)))
+   ivy-ignore-buffers '("company-statistics-cache.el" ".elfeed/index"))
   :diminish ivy-mode)
 
 (use-package ivy-hydra                  ; Additional bindings for Ivy
