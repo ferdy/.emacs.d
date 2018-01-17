54d9e98046befd84a871fcca1457e075d3bd44ed
Manuel Uberti <manuel.uberti@inventati.org>
2018-01-17 09:51:34 +0100

    Bind eval-buffer to C-c C-k in emacs-lisp-mode
    CIDER spoiled me.

--- lisp/mu-programming.el
+++ lisp/mu-programming.el
@@ -20,10 +20,11 @@
   :defer t
   :interpreter ("emacs" . emacs-lisp-mode)
   :bind (:map emacs-lisp-mode-map
-              ("C-c m e r" . eval-region)
+              ("C-c C-k" . eval-buffer)
               ("C-c m e b" . eval-buffer)
+              ("C-c m e f" . eval-defun)
               ("C-c m e e" . eval-last-sexp)
-              ("C-c m e f" . eval-defun))
+              ("C-c m e r" . eval-region))
   :config
   (defconst mu-use-package-imenu-expression
     `("Use Package" ,(rx "(use-package" (optional "-with-elapsed-timer")
