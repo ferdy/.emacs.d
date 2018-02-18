c3863f4a891c5d128043d980b697b5ad7635aa74
Manuel Uberti <manuel.uberti@inventati.org>
2018-02-18 08:10:25 +0100

    Remove Info-directory-list re-initialization

--- init.el
+++ init.el
@@ -101,16 +101,7 @@
                    "CARGO_HOME"         ; Cargo home, for racer
                    ))
 
-  (exec-path-from-shell-initialize)
-
-  ;; Re-initialize the `Info-directory-list' from $INFOPATH.  Since package.el
-  ;; already initializes info, we need to explicitly add the $INFOPATH
-  ;; directories to `Info-directory-list'.  We reverse the list of info paths
-  ;; to prepend them in proper order subsequently
-  (with-eval-after-load 'info
-    (dolist (dir (nreverse (parse-colon-path (getenv "INFOPATH"))))
-      (when dir
-        (add-to-list 'Info-directory-list dir)))))
+  (exec-path-from-shell-initialize))
 
 ;; Personal informations
 (validate-setq user-full-name (getenv "FULLNAME"))
