a979b7418bd33c9ddb87ec193c8195dae0c207aa
Manuel Uberti <manuel.uberti@inventati.org>
2018-03-25 10:28:30 +0200

    Add whole-line-or-region
    Prefer this package to custom macro

--- lisp/mu-editing.el
+++ lisp/mu-editing.el
@@ -241,6 +241,10 @@
   :ensure t
   :bind ("C-c x s" . string-edit-at-point))
 
+(use-package whole-line-or-region       ; Operate on line if region is undefined
+  :ensure t
+  :init (add-hook 'after-init-hook #'whole-line-or-region-mode))
+
 ;; Disable tabs, but given them proper width
 (setq-default indent-tabs-mode nil
               tab-width 8)
@@ -264,29 +268,6 @@
 ;;; Utilities and key bindings
 (bind-key "C-c x i" #'indent-region)
 (bind-key "C-c t v" #'visual-line-mode)
-
-;; Kill entire line with prefix argument
-(defmacro bol-with-prefix (function)
-  "Define a new function which will call FUNCTION.
-Except it moves to beginning of line before calling FUNCTION when
-called with a prefix argument.  The FUNCTION still receives the
-prefix argument."
-  (let ((name (intern (format "mu-%s-BOL" function))))
-    `(progn
-       (defun ,name (p)
-         ,(format
-           "Call `%s', but move to BOL when called with a prefix argument."
-           function)
-         (interactive "P")
-         (when p
-           (forward-line 0))
-         (call-interactively ',function))
-       ',name)))
-
-(bind-key [remap sp-kill-hybrid-sexp] (bol-with-prefix sp-kill-hybrid-sexp))
-(bind-key [remap org-kill-line] (bol-with-prefix org-kill-line))
-(bind-key [remap kill-line] (bol-with-prefix kill-line))
-(bind-key "C-k" (bol-with-prefix kill-visual-line))
 
 ;;;###autoload
 (defun just-one-space-in-region (beg end)
@@ -380,7 +361,7 @@ With prefix ARG, kill that many lines."
 
 ;;;###autoload
 (defun mu-smart-open-line ()
-  "Insert empty line after the current line."
+  "Move to end of line, enter a newline, and reindent."
   (interactive)
   (move-end-of-line 1)
   (newline-and-indent))
@@ -462,7 +443,7 @@ With arg N, insert N newlines."
  ([remap kill-whole-line]        . mu-smart-kill-whole-line)
  ([remap move-beginning-of-line] . mu-back-to-indentation-or-beginning-of-line)
  ("RET"                          . newline-and-indent)
- ("S-RET"                        . mu-smart-open-line)
+ ("C-c RET"                      . mu-smart-open-line)
  ("C-o"                          . mu-open-line-with-reindent)
  ("C-<backspace>"                . mu-smart-backward-kill-line))
 
