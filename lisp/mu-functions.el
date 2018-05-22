01b5d89fdf60932c2841033b5c12b0c63069cafb
Manuel Uberti <manuel.uberti@inventati.org>
2018-05-22 09:34:32 +0200

    Minor refactoring

--- lisp/mu-functions.el
+++ lisp/mu-functions.el
@@ -60,21 +60,25 @@ With negative argument, convert previous words."
                   (get-char-property pos 'face))))
     (if face (message "Face: %s" face) (message "No face at %d" pos))))
 
+(defun mu--os-version ()
+  "Call `lsb_release' to retrieve OS version."
+  (replace-regexp-in-string
+   "[ \t\n\r]+" " "
+   (with-temp-buffer
+     (and (eq 0
+              (call-process "lsb_release" nil '(t nil) nil "-d"))
+          (buffer-string)))))
+
 ;;;###autoload
 (defun mu-display-version ()
-  "Display Emacs version and system details in a temporary buffer.
-It requires `s.el'."
+  "Display Emacs version and system details in a temporary buffer."
   (interactive)
-  (let ((buffer-name "*version*")
-        (lsb (with-temp-buffer
-               (and (eq 0
-                        (call-process "lsb_release" nil '(t nil) nil "-d"))
-                    (buffer-string)))))
+  (let ((buffer-name "*version*"))
     (with-help-window buffer-name
       (with-current-buffer buffer-name
         (insert (emacs-version) "\n")
         (insert "\nRepository revision: " emacs-repository-version "\n")
-        (insert "\nSystem " (s-collapse-whitespace lsb) "\n")
+        (insert "\nSystem " (mu--os-version) "\n")
         (insert "\nWindowing system distributor `" (x-server-vendor)
                 "', version "
                 (mapconcat 'number-to-string (x-server-version) ".") "\n")
