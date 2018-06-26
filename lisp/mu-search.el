fd2f9f3b28216e33d5685df2a8938ceb214a5e8d
Manuel Uberti <manuel.uberti@inventati.org>
2018-06-26 08:03:17 +0200

    Remove deadgrep
    counsel-rg is usually enough.

--- lisp/mu-search.el
+++ lisp/mu-search.el
@@ -35,10 +35,6 @@
   :bind (([remap query-replace] . ez-query-replace)
          ("C-c M-%" . ez-query-replace-repeat)))
 
-(use-package deadgrep                   ; Search with ripgrep
-  :load-path "~/githubs/deadgrep"
-  :bind ("<f7>" . deadgrep))
-
 (provide 'mu-search)
 
 ;; Local Variables:
