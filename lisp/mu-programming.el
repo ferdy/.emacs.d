02481d0d82e5904f170754078440e5f4b8f713f5
Manuel Uberti <manuel.uberti@inventati.org>
2018-07-04 08:38:35 +0200

    Colourize CSS colours in HTML files

--- lisp/mu-programming.el
+++ lisp/mu-programming.el
@@ -304,7 +304,34 @@
 
 (use-package css-mode                   ; Better CSS support
   :defer t
-  :config (validate-setq css-indent-offset 2))
+  :config
+  (validate-setq css-indent-offset 2)
+
+  ;; Embedding in HTML
+  (with-eval-after-load 'mmm-vars
+    (mmm-add-group
+     'html-css
+     '((css-cdata
+        :submode css-mode
+        :face mmm-code-submode-face
+        :front "<style[^>]*>[ \t\n]*\\(//\\)?<!\\[CDATA\\[[ \t]*\n?"
+        :back "[ \t]*\\(//\\)?]]>[ \t\n]*</style>"
+        :insert ((?j js-tag nil @ "<style type=\"text/css\">"
+                     @ "\n" _ "\n" @ "</style>" @)))
+       (css
+        :submode css-mode
+        :face mmm-code-submode-face
+        :front "<style[^>]*>[ \t]*\n?"
+        :back "[ \t]*</style>"
+        :insert ((?j js-tag nil @ "<style type=\"text/css\">"
+                     @ "\n" _ "\n" @ "</style>" @)))
+       (css-inline
+        :submode css-mode
+        :face mmm-code-submode-face
+        :front "style=\""
+        :back "\"")))
+    (dolist (mode (list 'html-mode 'nxml-mode))
+      (mmm-add-mode-ext-class mode "\\.r?html\\(\\.erb\\)?\\'" 'html-css))))
 
 (use-package css-eldoc                  ; Eldoc for CSS
   :ensure t
