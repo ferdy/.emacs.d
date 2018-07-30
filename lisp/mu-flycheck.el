b29720d668c8233aff49faa1422508ac15fa888f
Manuel Uberti <manuel.uberti@inventati.org>
2018-07-30 08:27:12 +0200

    Add handy hydra for Flycheck
    See: https://www.reddit.com/r/emacs/comments/931la6/tip_how_to_adopt_flycheck_as_your_new_best_friend/

--- lisp/mu-flycheck.el
+++ lisp/mu-flycheck.el
@@ -12,7 +12,8 @@
 ;; Requires: chktex
 (use-package flycheck                   ; On-the-fly syntax checker
   :ensure t
-  :bind ("C-c t e" . flycheck-mode)
+  :bind (("C-c t e" . flycheck-mode)
+         ("C-c e"   . hydra-flycheck/body))
   :init
   (defun mu-flycheck-set-load-path-for-user-configuration ()
     "Set Flycheck load path for files in user configuration."
@@ -37,6 +38,29 @@
             #'mu-discard-undesired-html-tidy-error)
   (add-hook 'flycheck-mode-hook
             #'mu-flycheck-set-load-path-for-user-configuration)
+  
+  (defhydra hydra-flycheck (:color blue)
+    "
+  ^
+  ^Flycheck^          ^Errors^            ^Checker^
+  ^────────^──────────^──────^────────────^───────^─────
+  _q_ quit            _<_ previous        _?_ describe
+  _M_ manual          _>_ next            _d_ disable
+  _v_ verify setup    _f_ check           _m_ mode
+  ^^                  _l_ list            _s_ select
+  ^^                  ^^                  ^^
+  "
+    ("q" nil)
+    ("<" flycheck-previous-error :color pink)
+    (">" flycheck-next-error :color pink)
+    ("?" flycheck-describe-checker)
+    ("M" flycheck-manual)
+    ("d" flycheck-disable-checker)
+    ("f" flycheck-buffer)
+    ("l" flycheck-list-errors)
+    ("m" flycheck-mode)
+    ("s" flycheck-select-checker)
+    ("v" flycheck-verify-setup))
 
   (global-flycheck-mode))
 
