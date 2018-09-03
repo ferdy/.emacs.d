572666c7f7cc6cb3a1fd7c1be05df7da26176e63
Manuel Uberti <manuel.uberti@inventati.org>
2018-09-03 10:52:24 +0200

    Prettify some symbols in Lisp-dialects

--- lisp/mu-style.el
+++ lisp/mu-style.el
@@ -105,7 +105,31 @@
   (global-prettify-symbols-mode 1)
 
   ;; Unprettify symbols with point on them and next to them
-  (validate-setq prettify-symbols-unprettify-at-point 'right-edge))
+  (validate-setq prettify-symbols-unprettify-at-point 'right-edge)
+
+  ;; Add some pretty symbols to Clojure and Lisp modes
+  (defvar mu-clojure-prettify-alist '())
+
+  (add-to-list 'mu-clojure-prettify-alist
+               '("<=" . (?· (Br . Bl) ?≤)))
+  (add-to-list 'mu-clojure-prettify-alist
+               '(">=" . (?· (Br . Bl) ?≥)))
+  (add-to-list 'mu-clojure-prettify-alist
+               '("->" . (?- (Br . Bc) ?- (Br . Bc) ?>)))
+  (add-to-list 'mu-clojure-prettify-alist
+               '("->>" .  (?\s (Br . Bl) ?\s (Br . Bl) ?\s
+                               (Bl . Bl) ?- (Bc . Br) ?- (Bc . Bc) ?>
+                               (Bc . Bl) ?- (Br . Br) ?>)))
+
+  (with-eval-after-load 'clojure-mode
+    (validate-setq clojure--prettify-symbols-alist
+                   (append mu-clojure-prettify-alist
+                           clojure--prettify-symbols-alist)))
+
+  (with-eval-after-load 'lisp-mode
+    (validate-setq lisp-prettify-symbols-alist
+                   (append mu-clojure-prettify-alist
+                           lisp-prettify-symbols-alist))))
 
 (use-package ansi-color                 ; Colorize ANSI escape sequences
   :defer t
