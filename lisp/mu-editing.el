ad728e307eb2443287683906030e788dcded8e5a
Manuel Uberti <manuel.uberti@inventati.org>
2018-09-18 15:26:58 +0200

    Change binding for buffer cleanup function
    I use this often enough to need a better binding.

--- lisp/mu-editing.el
+++ lisp/mu-editing.el
@@ -394,7 +394,7 @@ Including `indent-buffer', which should not be called automatically on save."
   (whitespace-cleanup)
   (indent-buffer))
 
-(bind-key "C-c t c" #'cleanup-buffer)
+(bind-key "C-c C-b" #'cleanup-buffer)
 
 ;;;###autoload
 (defun mu-smart-kill-whole-line (&optional arg)
