24680974037dae5f85cbe2a65a2bf0f5d4996794
Manuel Uberti <manuel.uberti@inventati.org>
2018-09-26 09:47:17 +0200

    Tweak Ibuffer settings

--- lisp/mu-buffers.el
+++ lisp/mu-buffers.el
@@ -102,7 +102,7 @@
 (use-package ibuf-ext                   ; Extensions for Ibuffer
   :config
   ;; Do not show empty groups
-  (validate-setq ibuffer-show-empty-filter-groups nil))
+  (setq-default ibuffer-show-empty-filter-groups nil))
 
 (use-package ibuffer                    ; Buffer management
   :bind (("C-x C-b" . mu-ibuffer-open)
@@ -123,6 +123,13 @@
   (with-eval-after-load 'ibuffer
     (fullframe ibuffer mu-pop-window-configuration))
 
+  (defun mu-ibuffer-set-up-preferred-filters ()
+    (ibuffer-vc-set-filter-groups-by-vc-root)
+    (unless (eq ibuffer-sorting-mode 'filename/process)
+      (ibuffer-do-sort-by-filename/process)))
+
+  (add-hook 'ibuffer-hook #'mu-ibuffer-set-up-preferred-filters)
+
   ;; Use human readable Size column instead of original one
   (define-ibuffer-column size-h
     (:name "Size" :inline t)
@@ -130,7 +137,7 @@
      ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
      ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
      (t (format "%8d" (buffer-size)))))
-  
+
   (validate-setq ibuffer-formats
                  '((mark modified read-only " "
                          (name 35 35 :left :elide)
@@ -143,7 +150,7 @@
                    (mark modified read-only " "
                          (name 35 35 :left :elide)
                          " "
-                         (size 9 -1 :right)
+                         (size-h 9 -1 :right)
                          " "
                          (mode 16 16 :left :elide)
                          " "
