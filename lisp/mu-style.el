51eb8e333947086a2665cc18decc0dfc95548afb
Manuel Uberti <manuel.uberti@inventati.org>
2018-10-02 09:29:08 +0200

    Remove moody-related customizations

--- lisp/mu-style.el
+++ lisp/mu-style.el
@@ -150,14 +150,7 @@
 (use-package color-theme-sanityinc-tomorrow ; Default theme
   :ensure t
   :config
-  (load-theme 'sanityinc-tomorrow-night 'no-confirm)
-
-  (let ((line (face-attribute 'mode-line :underline)))
-    (set-face-attribute 'mode-line nil :overline line)
-    (set-face-attribute 'mode-line-inactive nil :overline line)
-    (set-face-attribute 'mode-line-inactive nil :underline line)
-    (set-face-attribute 'mode-line nil :box nil)
-    (set-face-attribute 'mode-line-inactive nil :box nil)))
+  (load-theme 'sanityinc-tomorrow-night 'no-confirm))
 
 (use-package dimmer                     ; Highlight selected buffer
   :ensure t
