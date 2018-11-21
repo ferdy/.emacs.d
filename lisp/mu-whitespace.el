3a19f94976af6cf4d9e9b89d784d8f864ce29557
Manuel Uberti <manuel@7bridges.eu>
2018-11-21 10:46:19 +0100

    Never show trailing whitespaces
    I clean them all up on save anyway.

--- lisp/mu-whitespace.el
+++ lisp/mu-whitespace.el
@@ -17,23 +17,6 @@
   :bind (("C-c <backspace>" . hungry-delete-backward)
          ("C-c <deletechar>" . hungry-delete-forward)))
 
-(setq-default show-trailing-whitespace t)
-
-(defun mu-no-trailing-whitespace ()
-  "Turn off display of trailing whitespace in the current buffer."
-  (setq show-trailing-whitespace nil))
-
-(dolist (hook '(calendar-mode
-                cider-repl-mode-hook
-                comint-mode-hook
-                compilation-mode-hook
-                eww-mode-hook
-                minibuffer-setup-hook
-                special-mode-hook
-                term-mode-hook
-                Info-mode-hook))
-  (add-hook hook #'mu-no-trailing-whitespace))
-
 (provide 'mu-whitespace)
 
 ;; Local Variables:
