33a42832e166789ce05a541d54d3f2bc4bdb5ab7
Manuel Uberti <manuel@7bridges.eu>
2018-11-21 10:44:10 +0100

    Try a different command from flyspell-correct-ivy

--- lisp/mu-languages.el
+++ lisp/mu-languages.el
@@ -97,7 +97,7 @@ Change dictionary and mode line lighter accordingly."
   :ensure t
   :after flyspell
   :bind (:map flyspell-mode-map
-              ("C-c $" . flyspell-correct-wrapper)))
+              ("C-c $" . flyspell-correct-at-point)))
 
 ;;; Dictionaries and synonyms
 (use-package wordnut                    ; Interface to WordNet
